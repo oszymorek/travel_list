6e173d816fe14da24b145259200e50be6157ede4
Szymorek Oskar <oskar_szymorek@upl.local>
2024-08-14 10:41:08 +0200

    Project#04 Travel List

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    );
+  }
+
+  function handleDeleteArray() {
+    const confirmed = window.confirm("Are you sure to clear all items?");
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onChangeItem={handleToggleItem}
+        onDeleteArray={handleDeleteArray}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
